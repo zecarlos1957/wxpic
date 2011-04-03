cb8a72c7b6cb4e0eb6a1a09c992f5853edb81395
giles314 <giles314@c1a92d2c-9611-4a04-95c1-05e219d6ec85>
2011-04-03 17:52:38 +0000

    Creation of V1.2.2 label.
    git-svn-id: svn://svn.code.sf.net/p/wxpic/code@122 c1a92d2c-9611-4a04-95c1-05e219d6ec85

---
+++ Tags/V1.2.2/src/CodeMemPanel.cpp
@@ -0,0 +1,88 @@
+#include "CodeMemPanel.h"
+#include "MainFrame.h"
+
+//(*InternalHeaders(TCodeMemPanel)
+#include <wx/intl.h>
+#include <wx/string.h>
+//*)
+
+#include <WinPicPr/PIC_HEX.h>
+
+//(*IdInit(TCodeMemPanel)
+const long TCodeMemPanel::ID_CODE_MEM_GRID = wxNewId();
+//*)
+
+BEGIN_EVENT_TABLE(TCodeMemPanel,wxPanel)
+	//(*EventTable(TCodeMemPanel)
+	//*)
+END_EVENT_TABLE()
+
+TCodeMemPanel::TCodeMemPanel(wxWindow* parent,wxWindowID id,const wxPoint& pos,const wxSize& size, long style, const wxString& name)
+{
+	//(*Initialize(TCodeMemPanel)
+	wxBoxSizer* BoxSizer5;
+
+	Create(parent, id, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL, _T("id"));
+	BoxSizer5 = new wxBoxSizer(wxHORIZONTAL);
+	aCodeMemGrid = new THexGrid(this, ID_CODE_MEM_GRID, wxDefaultPosition, wxDefaultSize, 0, _T("ID_CODE_MEM_GRID"));
+	aCodeMemGrid->CreateGrid(0,8);
+	aCodeMemGrid->SetMinSize(wxSize(0,0));
+	BoxSizer5->Add(aCodeMemGrid, 1, wxALL|wxEXPAND|wxALIGN_LEFT|wxALIGN_TOP, 0);
+	SetSizer(BoxSizer5);
+	BoxSizer5->Fit(this);
+	BoxSizer5->SetSizeHints(this);
+	//*)
+}
+
+TCodeMemPanel::~TCodeMemPanel()
+{
+	//(*Destroy(TCodeMemPanel)
+	//*)
+}
+
+
+
+//---------------------------------------------------------------------------
+void TCodeMemPanel::UpdateCodeMemDisplay(void)
+{
+    wxString old_msg;
+    old_msg = MainFrame::TheMainFrame->aStatusBar->GetStatusText();
+    MainFrame::TheMainFrame->aStatusBar->SetStatusText(_("Updating CODE display..")) ;
+    MainFrame::TheMainFrame->Update();
+    MainFrame::TheMainFrame->m_update_code_mem_display = false;
+
+
+    aCodeMemGrid->SetDefaultCellBackgroundColour (MainFrame::TheMainFrame->aOptionTab->aCodeMemColourButton->GetBackgroundColour());
+    aCodeMemGrid->SetDefaultCellTextColour       (MainFrame::TheMainFrame->aOptionTab->aCodeMemColourButton->GetForegroundColour());
+
+    //  Speciality for PICs with so-called EXECUTIVE MEMORY (kind of 'bootloader'):
+    //  - If the executive code buffer is loaded, only show the 'ordinary' memory
+    //    locations which really contain 'something' .
+    //  - If the executive code buffer is not loaded, don't show the executive memory
+    //    at all, but instead show ALL locations in the 'ordinary' code buffer .
+    int TotalLocation = std::min(PIC_DeviceInfo.lCodeMemSize, (long)PicBuf[PIC_BUF_CODE].dwMaxSize);
+    aCodeMemGrid->SetHexFormat(PIC_DeviceInfo.iBitsPerInstruction);
+    if ( PicBuf[PIC_BUF_EXEC].dwMaxSize>0 && PicBuf[PIC_BUF_EXEC].i32LastUsedArrayIndex>0 )
+    {
+        // there seems to be "loaded" EXECUTIVE CODE MEMORY -> show multiple sections !
+        if ( PicBuf[PIC_BUF_CODE].i32LastUsedArrayIndex >=/*!*/ 0 )
+            aCodeMemGrid->LoadHexData (aCodeMemAddrGetter1.SetMemory(PIC_BUF_CODE, TotalLocation), TotalLocation, aCodeMemAddrGetter1, _T("User Code Memory"), 0);
+        else
+            aCodeMemGrid->UnloadData(0);
+        int TotalExecLocation = std::min(PicBuf[PIC_BUF_EXEC].i32LastUsedArrayIndex+1, (long)PicBuf[PIC_BUF_EXEC].dwMaxSize);
+        aCodeMemGrid->LoadHexData (aCodeMemAddrGetter2.SetMemory(PIC_BUF_EXEC, TotalExecLocation), TotalExecLocation, aCodeMemAddrGetter2, _T("Executive Code Memory"), 1);
+    }
+    else // no EXECUTIVE MEMORY, only 'ordinary' code memory : No need to show what it is !
+    {
+        aCodeMemGrid->LoadHexData (aCodeMemAddrGetter1.SetMemory(PIC_BUF_CODE, TotalLocation), TotalLocation, aCodeMemAddrGetter1);
+        aCodeMemGrid->UnloadData(1);
+        aCodeMemAddrGetter2.Invalidate();
+    }
+
+    MainFrame::TheMainFrame->aStatusBar->SetStatusText(old_msg);
+    MainFrame::TheMainFrame->Update();
+
+} // end UpdateCodeMemDisplay()
+//---------------------------------------------------------------------------
+
+
---
+++ Tags/V1.2.2/src/ConfigMemoryPanel.h
@@ -0,0 +1,68 @@
+#ifndef CONFIGMEMORYPANEL_H
+#define CONFIGMEMORYPANEL_H
+
+//(*Headers(TConfigMemoryPanel)
+#include <wx/sizer.h>
+#include <wx/stattext.h>
+#include <wx/radiobox.h>
+#include <wx/checkbox.h>
+#include <wx/panel.h>
+#include <wx/grid.h>
+#include <wx/button.h>
+//*)
+
+class TConfigMemoryPanel: public wxPanel
+{
+	public:
+
+		TConfigMemoryPanel(wxWindow* parent,wxWindowID id,const wxPoint& pos,const wxSize& size, long style, const wxString& name);
+		virtual ~TConfigMemoryPanel();
+
+		//(*Declarations(TConfigMemoryPanel)
+		wxStaticText* StaticText10;
+		wxCheckBox* aShowAllCfgCellsChk;
+		wxStaticText* StaticText11;
+		wxStaticText* aDevId;
+		wxGrid* aCfgMemGrid;
+		wxStaticText* aDevIdTitle;
+		wxButton* aHelpOnIdButton;
+		wxPanel* Panel6;
+		wxButton* aApplyIdLocsButton;
+		wxStaticText* aDevIdDecoded;
+		wxRadioBox* aIdBinHexRadio;
+		//*)
+        char     m_sz80DetectedPicDevName[82];
+
+        void ApplyIdLocationDisplay(void);
+        void UpdateIdAndConfMemDisplay(void);
+
+	protected:
+
+		//(*Identifiers(TConfigMemoryPanel)
+		static const long ID_CFG_MEM_GRID;
+		static const long ID_STATICTEXT10;
+		static const long ID_ID_BIN_HEX_RADIO;
+		static const long ID_SHOW_ALL_CFG_CELLS_CHK;
+		static const long ID_STATICTEXT11;
+		static const long ID_DEV_ID_TITLE;
+		static const long ID_DEV_ID;
+		static const long ID_DEV_ID_DECODED;
+		static const long ID_APPLY_ID_LOCS_BUTTON;
+		static const long ID_BUTTON6;
+		static const long ID_PANEL6;
+		//*)
+
+	private:
+
+		//(*Handlers(TConfigMemoryPanel)
+		void onCfgMemGridCellChange(wxGridEvent& event);
+		void onIdBinHexRadioSelect(wxCommandEvent& event);
+		void onShowAllCfgCellsChkClick(wxCommandEvent& event);
+		void onApplyIdLocsButtonClick(wxCommandEvent& event);
+		void onHelpOnIdButtonClick(wxCommandEvent& event);
+		//*)
+
+		DECLARE_EVENT_TABLE()
+};
+
+#endif
