9e2f11ae600fcad0c63230563de45e568a4080b8
giles314 <giles314@c1a92d2c-9611-4a04-95c1-05e219d6ec85>
2012-05-28 11:19:48 +0000

    #0000060: Add verification that WxPic has not been started in a dangerous environment (sudo without -H)
    git-svn-id: svn://svn.code.sf.net/p/wxpic/code@208 c1a92d2c-9611-4a04-95c1-05e219d6ec85

--- Branches/ToV1.3/src/WxApp.cpp
+++ Branches/ToV1.3/src/WxApp.cpp
@@ -17,6 +17,12 @@
 #include "Language.h"
 #include <../resources/Resource.h>
 
+#ifndef __WXMSW__
+#include <wx/msgdlg.h>
+#include <unistd.h>
+#include <pwd.h>
+#endif // ndef __WXMSW__
+
 //(*AppHeaders
 #include <wx/image.h>
 //*)
@@ -29,6 +35,42 @@ int BreakPoint (void)
 }
 int BreakOnInit = BreakPoint();
 #endif
+
+#ifndef __WXMSW__
+static bool CheckUserID (void)
+{
+    if (!iopl(3)==0)
+        //-- If we fail to get the IO privileges there is no risk of problem with UID
+        return  true;
+
+    //-- return to normal privilege level
+    iopl(0);
+
+    //--Get the User UID
+    uid_t       Uid = getuid();
+    struct stat HomeStat;
+    const char *HomeDir  = getenv("HOME");
+    stat(HomeDir, &HomeStat);
+    bool Result = (Uid == HomeStat.st_uid);
+    if (!Result)
+    {
+        const passwd *UserData = getpwuid(Uid);
+        const char   *UserName = strdup(UserData->pw_name);
+        UserData = getpwuid(HomeStat.st_uid);
+        Result = (wxMessageBox(wxString::Format(_("WxPic is running with priviledges under %hs account (UID=%u)"
+                                                  " but with home directory (%hs) that belongs to %hs (UID=%u).\n"
+                                                  "This discrepency may cause WxPic and Browser file protection corruption."
+                                                  " This is generally caused by the use of sudo without -H option."
+                                                  " WxPic will abort to avoid corruption. Click No to continue (not recommended)" ),
+                                                UserName, Uid, HomeDir, UserData->pw_name, HomeStat.st_uid),
+                               _("Invalid Execution Environment"),
+                               wxYES_NO | wxICON_EXCLAMATION )
+                  == wxYES);
+        delete  UserName;
+    }
+    return Result;
+}
+#endif // ndef __WXMSW__
 
 class MyApp : public wxApp
 {
@@ -43,15 +85,18 @@ IMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit()
 {
-    bool wxsOK = true;
-
     //(*AppInitialize
     wxInitAllImageHandlers();
     //*)
 
     TResource::Load();
 
-    wxsOK = TSessionConfig::Init(this);
+#ifndef __WXMSW__
+    if (!CheckUserID())
+        return false;
+#endif // ndef __WXMSW__
+
+    bool wxsOK = TSessionConfig::Init(this);
 
     if (wxsOK)
         SetTopWindow(MainFrame::CreateAndShow());
