16b7b817fc284dfd7bc27d5822bbdce09cc0204e
giles314 <giles314@c1a92d2c-9611-4a04-95c1-05e219d6ec85>
2012-05-28 16:26:37 +0000

    Delete incorrect branch
    git-svn-id: svn://svn.code.sf.net/p/wxpic/code@219 c1a92d2c-9611-4a04-95c1-05e219d6ec85

--- V1.2.2/Tools.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Run Tools from Project Home Directory
-REM with Project name as parameter
-REM Allows to apply generic tools to the project
-REM If no generic tools exists, do nothing
-if exist %TOOLS_DIR%\Bin\Tools.bat call %TOOLS_DIR%\Bin\Tools.bat WxPic
-pause
